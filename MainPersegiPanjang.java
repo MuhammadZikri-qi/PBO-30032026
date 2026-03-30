3a1005c156a9e96b8ebfcb2f6f4bae89fd56dd35
Muhammad Zikri <muhammadzikri021106@gmail.com>
2026-03-30 22:17:42 +0700

    Remove commented license and clean up code

--- MainPersegiPanjang.java
+++ MainPersegiPanjang.java
@@ -1,23 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package pratikum30032026;
 
 import java.util.Scanner;
 
-/**
- *
- * @author LAB-SI-PC
- */
 public class MainPersegiPanjang {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        
-        // TODO code application logic here
         
         PersegiPanjang pp1 = new PersegiPanjang();
         pp1.panjang = 20;
